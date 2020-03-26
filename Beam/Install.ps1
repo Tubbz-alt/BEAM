d4a0d445c45100fcbe4b34f8c3ee10d2a30d6ad5
MKS DTECH <mksdesigntech@gmail.com>
2020-03-26 11:40:55 +0000

    Update Install.ps1

--- Beam/Install.ps1
+++ Beam/Install.ps1
@@ -12,6 +12,7 @@ try
     New-Item "$env:LOCALAPPDATA\Beam\Temp" -ItemType directory -Force | Out-Null
     New-Item "$env:LOCALAPPDATA\Beam\MKS" -ItemType directory -Force | Out-Null
     New-Item "$env:LOCALAPPDATA\Beam\MKS\bin" -ItemType directory -Force | Out-Null
+    New-Item "$env:LOCALAPPDATA\Beam\MKS\Templates" -ItemType directory -Force | Out-Null
 
     Remove-Item "HKCU:\Software\McNeel\Rhinoceros\6.0\Plug-ins\2ed06c48-9229-4f20-984e-ae31fe55e316" -Recurse -ErrorAction Ignore
 
@@ -28,7 +29,9 @@ try
     Copy-Item "Newtonsoft.Json.dll" "$env:LOCALAPPDATA\Beam\MKS\bin\" -ErrorAction Ignore
     Copy-Item "MKS\pkg.json" "$env:LOCALAPPDATA\Beam\MKS\" -ErrorAction Ignore
     Copy-Item "MKS\bin\MKS.dll" "$env:LOCALAPPDATA\Beam\MKS\bin\" -ErrorAction Ignore
-    Copy-Item "MKS\bin\MKS.customization.dll" "$env:LOCALAPPDATA\Beam\MKS\bin\" -ErrorAction Ignore    
+    Copy-Item "MKS\bin\MKS.customization.dll" "$env:LOCALAPPDATA\Beam\MKS\bin\" -ErrorAction Ignore
+    
+    Copy-Item -Path "Templates\*" -Destination "$env:LOCALAPPDATA\Beam\Templates\ -Recurse -ErrorAction Ignore
 }
 catch
 {
